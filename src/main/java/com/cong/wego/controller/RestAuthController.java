1854089ff2e19a618f132ffa3431c0a4c6620cac
lhccong <liuhuaicong@zhut.cn>
2024-05-17 10:16:16 +0800

    feat: 🚀新增 API 接口注释

--- src/main/java/com/cong/wego/controller/RestAuthController.java
+++ src/main/java/com/cong/wego/controller/RestAuthController.java
@@ -2,6 +2,8 @@ package com.cong.wego.controller;
 
 
 import com.cong.wego.config.GitHubConfig;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import me.zhyd.oauth.request.AuthRequest;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,11 +15,13 @@ import java.io.IOException;
 
 @RestController
 @RequestMapping("/oauth")
+@Api(tags = "Github 登录相关")
 public class RestAuthController {
     @Resource
     private GitHubConfig gitHubConfig;
 
     @RequestMapping("/render")
+    @ApiOperation(value = "获取GitHub授权地址重定向")
     public void renderAuth(HttpServletResponse response) throws IOException {
         AuthRequest authRequest = gitHubConfig.getAuthRequest();
         response.sendRedirect(authRequest.authorize("wegoGithub"));
